c6193f75f07b80a361efa1e527ea62e38c21338d
Eric Asongwe <ASER@contec-int.at>
2020-02-13 09:52:59 +0100

    Initial files

--- config/app.php
+++ config/app.php
@@ -51,7 +51,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'https://tokenqueue.cash2mobile.biz'),
+    'url' => env('APP_URL', 'localhost'),
 
     /*
     |--------------------------------------------------------------------------
